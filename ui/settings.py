dc1089dc5705ca676a8ccf0fb25eb2164908e4a6
edgar965 <e@edgarm.de>
2026-04-19 16:13:35 +0200

    Bump version to 0.38

--- ui/settings.py
+++ ui/settings.py
@@ -2,7 +2,7 @@ import os
 import sys
 from pathlib import Path
 
-VERSION = '0.37'
+VERSION = '0.38'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 TOOLS_ROOT = BASE_DIR.parent              # A:\3DTools
